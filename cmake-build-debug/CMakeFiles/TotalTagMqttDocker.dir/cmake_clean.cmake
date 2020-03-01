07e7105cfde5db99f1621a2d6d1ea9f0c045538c
Daniel Mendes <daniel@danim.com.br>
2020-03-01 09:24:37 -0300

    Status inicial do Projeto

---
+++ cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TotalTagMqttDocker.dir/main.cpp.o"
+  "TotalTagMqttDocker"
+  "TotalTagMqttDocker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TotalTagMqttDocker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
