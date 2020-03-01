66209bed06a5315ddb3767806ba1e8d58e5de6a5
Daniel Mendes <daniel@danim.com.br>
2020-03-01 11:53:22 -0300

    Pegando o datetime atual

--- TagInfo.h
+++ TagInfo.h
@@ -6,11 +6,14 @@
 #define TOTALTAGMQTTDOCKER_TAGINFO_H
 
 #include <string>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
 
 struct TagInfo
 {
+    time_t DateTime;
     string Epc;
     int AntennaPort;
     double Rssi;
@@ -18,6 +21,7 @@ struct TagInfo
 
     TagInfo(string& epc, int antennaPort, double rssi, double phase)
     :
+    DateTime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())),
     Epc(epc),
     AntennaPort(antennaPort),
     Rssi(rssi),
