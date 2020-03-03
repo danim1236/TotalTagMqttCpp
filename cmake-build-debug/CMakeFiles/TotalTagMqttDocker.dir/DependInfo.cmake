5b85202e362d78d97412c0f495177ceaec9a3aac
Daniel Mendes <daniel@danim.com.br>
2020-03-02 23:26:07 -0300

    Arquivos faltantes

---
+++ cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/danim/CLionProjects/TotalTagMqttDocker/EventManager.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/EventManager.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/MessageParser.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/MessageParser.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/MqttClient.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/MqttClient.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/TagInfoCache.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/TagInfoCache.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/TotalTagSender.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/TotalTagSender.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/Util.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/Util.cpp.o"
+  "/home/danim/CLionProjects/TotalTagMqttDocker/main.cpp" "/home/danim/CLionProjects/TotalTagMqttDocker/cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TotalTagMqttDocker.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TotalTagMqttDocker.dir/EventManager.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/MessageParser.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/MqttClient.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/TagInfoCache.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/TotalTagSender.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/Util.cpp.o"
+  "CMakeFiles/TotalTagMqttDocker.dir/main.cpp.o"
+  "TotalTagMqttDocker"
+  "TotalTagMqttDocker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TotalTagMqttDocker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
